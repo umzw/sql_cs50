9dd1b6615090399cd87b4f55ade594f5322b6f83
YukaUmezawa <90828055+YukaUmezawa@users.noreply.github.com>
2022-03-09 21:18:48 +0900

    220309

--- airline/flights/models.py
+++ airline/flights/models.py
@@ -20,6 +20,8 @@ class Flight(models.Model):
     def __str__(self):
         return f"{self.id}: {self.origin} to {self.destination}"
 
+    def is_valid_flight(self):
+        return self.origin != self.destination and self.duration>0
 
 class Passengers(models.Model):
     first=models.CharField(max_length=64)
--- airline/flights/tests.py
+++ airline/flights/tests.py
@@ -1,3 +1,47 @@
-from django.test import TestCase
+# from django.test import TestCase
 
 # Create your tests here.
+from django.db.models import Max
+from django.test import Client, TestCase
+
+from .models import Airport, Flight, Passengers
+
+class FlightTestCase(TestCase):
+
+    def setUp(self):
+
+        #Create Airports
+        a1=Airport.objects.create(code="AAA", city="City A")
+        a2=Airport.objects.create(code="BBB", city="City B")
+
+        #Create Flights
+        Flight.objects.create(origin=a1, destination=a2, duration=100)
+        Flight.objects.create(origin=a1, destination=a1, duration=200)
+        Flight.objects.create(origin=a1, destination=a2, duration=-100)
+
+    def test_departures_count(self):
+        a=Airport.objects.get(code="AAA")
+        self.assertEqual(a.departures.count(), 3)
+
+    def test_arrivals_count(self):
+        a=Airport.objects.get(code="AAA")
+        self.assertEqual(a.arrivals.count(), 1)
+
+    def test_valid_flight(self):
+        a1=Airport.objects.get(code="AAA")
+        a2=Airport.objects.get(code="BBB")
+        f=Flight.objects.get(origin=a1, destination=a2, duration=100)
+        self.assertTrue(f.is_valid_flight())
+
+
+    def test_invalid_flight_destination(self):
+        a1=Airport.objects.get(code="AAA")
+        f=Flight.objects.get(origin=a1, destination=a1)
+        self.assertFalse(f.is_valid_flight())
+
+
+    def test_invalid_flight_duration(self):
+        a1=Airport.objects.get(code="AAA")
+        a2=Airport.objects.get(code="BBB")
+        f=Flight.objects.get(origin=a1, destination=a2, duration=-100)
+        self.assertFalse(f.is_valid_flight())
